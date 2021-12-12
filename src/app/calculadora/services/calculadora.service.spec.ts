19f4bf9af3813d10cc0693ed2beed8bf9456911e
Kevin Ferreira <ferreirakevin325@gmail.com>
2021-12-12 05:56:40 -0300

    Criacao de testes unitarios

--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,6 +1,7 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
+
 
 describe('CalculadoraService', () => {
   let service: CalculadoraService;
@@ -10,7 +11,7 @@ describe('CalculadoraService', () => {
     service = TestBed.inject(CalculadoraService);
   });
 
-  it('should be created', () => {
+  it('should ...', inject([CalculadoraService], (service: CalculadoraService) => {
     expect(service).toBeTruthy();
-  });
+  }))
 });
